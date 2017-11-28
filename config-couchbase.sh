351831d7c707a914b36bf203beddbc274d4a2d8d
Juan Almeida <juanelojga@gmail.com>
2017-11-28 10:40:30 -0500

    Update to couchbase v5
    Update provision scripts
    Update couchbase node SDK package to last versions
    Update couchbase connector to allow rbac credentials

--- config-couchbase.sh
+++ config-couchbase.sh
@@ -51,60 +51,34 @@ curl --silent "http://${HOSTNAME}:8091/nodes/self/controller/settings" \
 numbered_echo "Setting hostname"
 curl --silent "http://${HOSTNAME}:8091/node/controller/rename" \
   -d hostname=${HOSTNAME}
-  
-if [[ ${CLUSTER_HOST} ]];then
-  numbered_echo "Joining cluster ${CLUSTER_HOST}"
-  curl --silent -u ${USERNAME}:${PASSWORD} \
-    "http://${CLUSTER_HOST}:8091/controller/addNode" \
-    -d hostname="${HOSTNAME}" \
-    -d user="${USERNAME}" \
-    -d password="${PASSWORD}" \
-    -d services="${SERVICES}" > /dev/null
 
-  if [[ ${CLUSTER_REBALANCE} ]]; then
-    # "Unexpected server error without the sleep 2
-    sleep 2
-    numbered_echo "Retrieving nodes"
-    known_nodes=$(
-      curl --silent -u ${USERNAME}:${PASSWORD} http://${CLUSTER_HOST}:8091/pools/default | read_nodes
-    )
+numbered_echo "Setting up memory"
+curl --silent "http://${HOSTNAME}:8091/pools/default" \
+  -d memoryQuota=${MEMORY_QUOTA} \
+  -d indexMemoryQuota=${INDEX_MEMORY_QUOTA} \
+  -d ftsMemoryQuota=${FTS_MEMORY_QUOTA}
 
-    numbered_echo "Rebalancing cluster"
-    curl -u ${USERNAME}:${PASSWORD} \
-      "http://${CLUSTER_HOST}:8091/controller/rebalance" \
-      -d knownNodes="${known_nodes}"
-  fi
+numbered_echo "Setting up services"
+curl --silent "http://${HOSTNAME}:8091/node/controller/setupServices" \
+  -d services="${SERVICES}"
 
-else
-  numbered_echo "Setting up memory"
-  curl --silent "http://${HOSTNAME}:8091/pools/default" \
-    -d memoryQuota=${MEMORY_QUOTA} \
-    -d indexMemoryQuota=${INDEX_MEMORY_QUOTA} \
-    -d ftsMemoryQuota=${FTS_MEMORY_QUOTA}
+numbered_echo "Setting up user credentials"
+curl --silent "http://${HOSTNAME}:8091/settings/web" \
+  -d port=8091 \
+  -d username=${WEB_USERNAME} \
+  -d password=${WEB_PASSWORD} > /dev/null
 
-  numbered_echo "Setting up services"
-  curl --silent "http://${HOSTNAME}:8091/node/controller/setupServices" \
-    -d services="${SERVICES}"
+numbered_echo "Setting indexer storage method"
+curl --silent -X POST "http://${HOSTNAME}:8091/settings/indexes" \
+  -u ${WEB_USERNAME}:${WEB_PASSWORD} \
+  -d "storageMode=${INDEX_STORAGE_MODE}"
 
-  numbered_echo "Setting up user credentials"
-  curl --silent "http://${HOSTNAME}:8091/settings/web" \
-    -d port=8091 \
-    -d username=${USERNAME} \
-    -d password=${PASSWORD} > /dev/null
-
-  numbered_echo "Setting indexer storage method"
-  curl --silent -X POST "http://${HOSTNAME}:8091/settings/indexes" \
-    -u ${USERNAME}:${PASSWORD} \
-    -d "storageMode=${INDEX_STORAGE_MODE}"
-
-fi
-
-# Remove the bucket
+numbered_echo "Remove the bucket"
 couchbase-cli bucket-delete -c ${HOSTNAME}:8091 \
   --bucket=${BUCKET_NAME} \
-  -u ${USERNAME} -p ${PASSWORD}
+  -u ${WEB_USERNAME} -p ${WEB_PASSWORD}
 
-# Create the bucket
+numbered_echo "Create the bucket: ${BUCKET_NAME}"
 couchbase-cli bucket-create -c ${HOSTNAME}:8091 \
   --bucket=${BUCKET_NAME} \
   --bucket-type=couchbase \
@@ -113,11 +87,18 @@ couchbase-cli bucket-create -c ${HOSTNAME}:8091 \
   --bucket-priority=high \
   --bucket-eviction-policy=valueOnly \
   --enable-flush=1 \
-  -u ${USERNAME} -p ${PASSWORD}
+  -u ${WEB_USERNAME} -p ${WEB_PASSWORD}
 
-sleep 30
+numbered_echo "Create RBAC user for ${BUCKET_NAME}"
+couchbase-cli user-manage -c ${HOSTNAME}:8091 -u ${WEB_USERNAME} -p ${WEB_PASSWORD} \
+  --set --rbac-username ${RBAC_USERNAME} --rbac-password ${RBAC_PASSWORD} \
+  --rbac-name ${RBAC_USERNAME} --roles bucket_admin[${BUCKET_NAME}] \
+  --auth-domain local
 
-echo "CREATE PRIMARY INDEX ON \`${BUCKET_NAME}\` USING GSI;" | cbq
+sleep 10
+
+numbered_echo "Create n1ql index for ${BUCKET_NAME}"
+echo "CREATE PRIMARY INDEX ON \`${BUCKET_NAME}\` USING GSI;" | cbq -u ${WEB_USERNAME} -p ${WEB_PASSWORD}
 
 # Attach to couchbase entrypoint
 numbered_echo "Attaching to couchbase-server entrypoint"
